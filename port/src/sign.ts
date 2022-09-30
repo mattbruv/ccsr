ad05504bcf37a56a7f652fad5254039b867662ff
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-09-30 15:37:31 -0400

    implement sign and set up styling

--- port/src/sign.ts
+++ port/src/sign.ts
@@ -3,8 +3,26 @@ import { Game, getMemberTexture } from "./game";
 export class GameSign {
   private game: Game;
   private sprite: PIXI.Sprite | undefined;
+  private textElement: HTMLParagraphElement;
+
   constructor(game: Game) {
     this.game = game;
+
+    // Initialize and style the text HTML element
+    this.textElement = document.createElement("p");
+    this.textElement.textContent = "hello world ".repeat(100);
+    this.textElement.style.position = "absolute";
+    this.textElement.style.top = "0";
+    this.textElement.style.left = "0";
+    this.textElement.style.backgroundColor = "white";
+    this.textElement.style.overflowY = "scroll";
+    this.textElement.style.display = "block";
+    this.textElement.style.border = "1px solid black";
+    this.textElement.style.userSelect = "none";
+    this.textElement.style.whiteSpace = "break-spaces"; // don't compress whitespace
+    this.textElement.style.fontFamily = "arial";
+
+    document.body.appendChild(this.textElement);
   }
 
   public init() {
@@ -20,5 +38,27 @@ export class GameSign {
     const x = Math.round(width / 2);
     const y = Math.round(height / 2);
     this.sprite?.position.set(x, y);
+
+    const scale = 1;
+    this.sprite?.scale.set(scale, scale);
+
+    this.textElement.style.fontSize = 100 * scale + "%";
+
+    const boxWidth = 242 * scale;
+    const boxHeight = 140 * scale;
+
+    const halfWidth = this.sprite!.width / 2;
+    const halfHeight = this.sprite!.height / 2;
+
+    const leftAdjust = 30 * scale;
+    const topAdjust = 30 * scale;
+
+    const left = this.sprite!.position.x - halfWidth + leftAdjust;
+    const top = this.sprite!.position.y - halfHeight + topAdjust;
+
+    this.textElement.style.width = boxWidth + "px";
+    this.textElement.style.height = boxHeight + "px";
+    this.textElement.style.left = left + "px";
+    this.textElement.style.top = top + "px";
   }
 }
