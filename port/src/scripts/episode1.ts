f95fef5b8e1caf8250cc9553633153c444cb153a
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-10-06 20:40:12 -0400

    tweak start pos

--- port/src/scripts/episode1.ts
+++ port/src/scripts/episode1.ts
@@ -25,7 +25,7 @@ export class Episode1 extends EpisodeScript {
   public init(): void {
     const startMap = "0106";
     this.game.setMap(startMap);
-    this.game.player.setMapAndPosition(startMap, 6, 10);
+    this.game.player.setMapAndPosition(startMap, 6, 8);
     this.game.camera.setScale();
     this.game.camera.snapCameraToMap(startMap);
 
