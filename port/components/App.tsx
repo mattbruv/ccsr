b501f9699e3163b69b5b23ea04f8495eb12cb685
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-10-14 12:12:21 -0400

    dark mode

--- port/components/App.tsx
+++ port/components/App.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import { Game } from "../src/game";
 import { SelectEpisode } from "./Select";
 import { Settings } from "./Settings";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 let game: Game; //  = new Game();
 
@@ -24,10 +32,11 @@ class App extends React.Component {
 
   render(): React.ReactNode {
     return (
-      <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
         <Settings />
         <SelectEpisode />
-      </>
+      </ThemeProvider>
       /*
       <div id="main">
         <div id="game-container"></div>
