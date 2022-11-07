0cc6e81ad6357465b7c5f9ecefa26f3edf7f715e
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-11-07 15:09:54 -0400

    animate smoke

--- port/src/scenes/scene3.ts
+++ port/src/scenes/scene3.ts
@@ -4,11 +4,38 @@ import { InventoryMode } from "../inventory";
 import { GameScene, MoveAnimation } from "../scene";
 import { Key } from "../types";
 
+class Smoke {
+  public sprite: PIXI.Sprite;
+  public age = 1;
+  public start = 0;
+  public end = 1;
+  public active = false;
+
+  constructor() {
+    this.sprite = new PIXI.Sprite(getMemberTexture("smoke"));
+    this.sprite.anchor.set(0.5);
+    this.sprite.scale.set(0.1);
+    this.sprite.visible = false;
+  }
+}
+
 export class Scene3 extends GameScene {
   public beach: PIXI.Container;
+  public volcan: PIXI.Sprite;
+  public boat: PIXI.Sprite;
+
+  public viv: PIXI.Sprite;
+  public courage: PIXI.Sprite;
+
+  public inBoat = true;
+  public erupting = true;
+  public smoke: Smoke[];
 
   constructor(game: Game) {
     super(game);
+
+    this.smoke = [];
+
     this.beach = new PIXI.Container();
 
     const mask = new PIXI.Graphics();
@@ -21,8 +48,14 @@ export class Scene3 extends GameScene {
     border.drawRect(0, 0, 416, 320);
 
     const sky = new PIXI.Sprite(getMemberTexture("background"));
-    const volcan = new PIXI.Sprite(getMemberTexture("volcano"));
+    this.volcan = new PIXI.Sprite(getMemberTexture("volcano"));
     const shore = new PIXI.Sprite(getMemberTexture("shoreline"));
+
+    this.boat = new PIXI.Sprite(getMemberTexture("VIV.COURAGE.BOAT"));
+    this.boat.anchor.set(0.5);
+    this.boat.position.set(323, 180);
+    this.boat.scale.set(0.2);
+
     const volcanShore = new PIXI.TilingSprite(getMemberTexture("tile.7")!);
     volcanShore.anchor.set(0.5);
     volcanShore.width = 200;
@@ -38,14 +71,14 @@ export class Scene3 extends GameScene {
     //sky.position.set(208, 160);
     sky.position.set(208, 135);
 
-    volcan.anchor.set(0.5);
-    volcan.position.set(329, 130);
+    this.volcan.anchor.set(0.5, 0.42);
+    this.volcan.position.set(327, 130);
 
     shore.anchor.set(0.5);
     shore.position.set(210, 304);
 
     this.beach.addChild(sky);
-    this.beach.addChild(volcan);
+    this.beach.addChild(this.volcan);
     this.beach.addChild(this.tree(228, 168));
     this.beach.addChild(this.tree(260, 168));
     this.beach.addChild(this.tree(286, 168));
@@ -66,6 +99,17 @@ export class Scene3 extends GameScene {
     this.beach.addChild(this.char("beetle", 383, 283));
     this.beach.addChild(this.char("block.42", 397, 312));
 
+    this.viv = this.char("player.normal.right.1", 206, 286);
+    this.viv.width = 29;
+    this.courage = this.char("courage.happy", 245, 286);
+    this.viv.visible = false;
+    this.courage.visible = false;
+
+    this.beach.addChild(this.viv);
+    this.beach.addChild(this.courage);
+
+    this.beach.addChild(this.boat);
+
     const t = this.tree(399, 168);
     t.width = 28;
     t.height = 27;
@@ -75,6 +119,79 @@ export class Scene3 extends GameScene {
     //this.container.addChild(border);
     this.container.addChild(mask);
     this.beach.mask = mask;
+
+    const boatToBeach: MoveAnimation = {
+      sprite: this.boat,
+      from: { x: 323, y: 180 },
+      to: { x: 230, y: 270 },
+      startFrame: 1,
+      endFrame: 27,
+    };
+
+    this.frameCallbacks.push({
+      frame: 28,
+      callback: () => {
+        this.inBoat = false;
+        this.boat.visible = false;
+        this.viv.visible = true;
+        this.courage.visible = true;
+      },
+    });
+
+    // 48 -> 115
+    // smoke starts at frame ~48 to frame ~114, 33 smokes every 2 frames
+    for (let i = 0; i < 33; i++) {
+      const start = i * 2 + 1;
+      const growEnd = start + 7;
+      const end = growEnd + 12;
+      const smoke = new Smoke();
+      smoke.start = start;
+      smoke.end = growEnd;
+      this.smoke.push(smoke);
+      this.frameCallbacks.push({
+        frame: start,
+        callback: () => {
+          smoke.active = true;
+          smoke.sprite.visible = true;
+        },
+      });
+      this.frameCallbacks.push({
+        frame: growEnd + 1,
+        callback: () => {
+          smoke.active = false;
+        },
+      });
+      this.frameCallbacks.push({
+        frame: end,
+        callback: () => {
+          smoke.sprite.visible = false;
+        },
+      });
+      this.moveAnims.push({
+        sprite: smoke.sprite,
+        from: { x: 335, y: 90 },
+        to: { x: 320, y: 44 },
+        startFrame: start,
+        endFrame: start + 7,
+      });
+      this.beach.addChild(smoke.sprite);
+      this.moveAnims.push({
+        sprite: smoke.sprite,
+        from: { x: 320, y: 44 },
+        to: { x: 228, y: -72 },
+        startFrame: start + 7,
+        endFrame: start + 7 + 12,
+      });
+    }
+
+    this.frameCallbacks.push({
+      frame: 75,
+      callback: () => {
+        this.erupting = false;
+      },
+    });
+
+    this.moveAnims.push(boatToBeach);
   }
 
   private char(member: string, x: number, y: number, flip?: boolean) {
@@ -102,5 +219,26 @@ export class Scene3 extends GameScene {
     this.playing = true;
   }
 
-  protected onFrame(): void {}
+  protected onFrame(): void {
+    if (this.erupting) {
+      const odd = this.currentFrame % 2 == 0;
+      const x = odd ? 327 : 329;
+      const y = odd ? 130 : 130;
+      this.volcan.position.set(x, y);
+    }
+
+    this.smoke
+      .filter((s) => s.active)
+      .map((s) => {
+        const scale = s.age / (s.end - s.start);
+        //console.log(s.start, s.age, s.end, scale);
+        s.age++;
+        s.sprite.scale.set(Math.min(1, scale));
+      });
+
+    if (this.inBoat) {
+      // update scale as it moves
+      this.boat.scale.set(Math.min(this.currentFrame / 21 + 0.2, 1));
+    }
+  }
 }
