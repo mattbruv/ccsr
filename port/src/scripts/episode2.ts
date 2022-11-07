8a187f2e98132358ca75bf3d9369133e93c20db0
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-11-07 10:27:39 -0400

    fix episode 2 bounds

--- port/src/scripts/episode2.ts
+++ port/src/scripts/episode2.ts
@@ -11,27 +11,35 @@ const maskHotel = new PIXI.Graphics();
 //this.game.worldContainer.mask = maskOverworld;
 
 export class Episode2 extends EpisodeScript {
-  onNewMap(nextMap: string): void {}
-
-  onDoor(nextMap: string): void {
+  onNewMap(nextMap: string): void {
     switch (nextMap) {
       case "0701":
       case "0702": {
+        this.game.camera.setCameraBounds("0701", "0702");
         this.game.viewport.mask = maskCrab;
         break;
       }
       case "0703":
       case "0704":
       case "0705": {
+        this.game.camera.setCameraBounds("0703", "0705");
         this.game.viewport.mask = maskHotel;
         break;
       }
+      case "0706": {
+        this.game.camera.setCameraBounds("0101", "0706");
+        this.game.viewport.mask = maskOverworld;
+        break;
+      }
       default: {
+        this.game.camera.setCameraBounds("0101", "0606");
         this.game.viewport.mask = maskOverworld;
         break;
       }
     }
   }
+
+  onDoor(nextMap: string): void {}
 
   public init(): void {
     let m: Rect = getMapsRect("0101", "0606");
