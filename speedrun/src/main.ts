d41ed6376371e7ad2bc7889b4038fca69bb863f1
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-11-17 19:19:22 -0400

    example code

--- speedrun/src/main.ts
+++ speedrun/src/main.ts
@@ -1,1 +1,45 @@
-import "./style.css";
+import cytoscape from "cytoscape";
+
+var cy = cytoscape({
+  container: document.getElementById("cy"), // container to render in
+  elements: [
+    // list of graph elements to start with
+    {
+      // node a
+      data: { id: "a" },
+    },
+    {
+      // node b
+      data: { id: "b" },
+    },
+    {
+      // edge ab
+      data: { id: "ab", source: "a", target: "b" },
+    },
+  ],
+  style: [
+    // the stylesheet for the graph
+    {
+      selector: "node",
+      style: {
+        "background-color": "#666",
+        label: "data(id)",
+      },
+    },
+    {
+      selector: "edge",
+      style: {
+        width: 3,
+        "line-color": "#ccc",
+        "target-arrow-color": "#ccc",
+        "target-arrow-shape": "triangle",
+      },
+    },
+  ],
+  layout: {
+    name: "grid",
+    rows: 1,
+  },
+});
+
+cy.resize();
