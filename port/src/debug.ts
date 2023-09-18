af648fc3b848526f10105c5bfab1586a594acaa3
Matt P <5638426+mattbruv@users.noreply.github.com>
2023-09-17 19:37:19 -0500

    draw rendering stuff

--- port/src/debug.ts
+++ port/src/debug.ts
@@ -39,8 +39,8 @@ export class Debugger {
     if (!import.meta.env.DEV) {
       return;
     }
-    //this.game.viewport.addChild(this.mapGrid);
-    //this.game.viewport.addChild(this.g);
+    this.game.viewport.addChild(this.mapGrid);
+    this.game.viewport.addChild(this.g);
 
     this.game.viewport.interactive = true;
 
