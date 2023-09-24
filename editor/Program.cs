8fb491d1b8e94f017907eddede82b1112829189c
Matt P <5638426+mattbruv@users.noreply.github.com>
2023-09-24 12:04:41 -0500

    commit

--- editor/Program.cs
+++ editor/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using editor;
+using Editor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
