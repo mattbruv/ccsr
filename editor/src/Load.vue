e3c0ef9b5c9615d3e051129f7c6e7a5b68d87c54
Matt P <5638426+mattbruv@users.noreply.github.com>
2023-10-04 23:38:15 -0500

    design shell for loading page

--- editor/src/Load.vue
+++ editor/src/Load.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
-//
+
+const episodeSelections = [
+    "Episode 1: Pool Problems",
+    "Episode 2: Tennis Menace",
+    "Episode 3: Vivian vs. the Volcano",
+    "Episode 4: Disco Dilema",
+    "Scooby Doo and the Hollywood Horror: Part 1",
+    "Scooby Doo and the Hollywood Horror: Part 2",
+]
+
 </script>
 
 <template>
     <v-container>
         <v-row>
             <v-col cols="12" md="6">
-                <v-card>
-                    <div>Select me</div>
+                <v-card style="padding: 1rem;">
+                    <v-card-title>Load From Episode</v-card-title>
+                    <v-card-subtitle>
+                        Load data from existing
+                        an existing episode.
+                    </v-card-subtitle>
+                    <div style="padding-top: 1rem;">
+                        <v-select label="Select Episode" :items="episodeSelections"></v-select>
+                    </div>
                 </v-card>
             </v-col>
             <v-col cols="12" md="6">
-                <v-card style="padding: 1rem;">
+                <v-card style="padding: 1rem;" variant="tonal">
                     <v-card-title>Load From Project File</v-card-title>
                     <v-card-subtitle>
                         Load a previously exported
