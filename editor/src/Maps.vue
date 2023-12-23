81975ff5bc2f1b037a8cb7f1d736228ff7ece9d4
Matt Pitts <matt.pitts@nuqleous.com>
2023-12-22 22:27:36 -0600

    heehe

--- editor/src/Maps.vue
+++ editor/src/Maps.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { useStore } from "./store";
 const store = useStore();
+const maps = computed(() => {
+  return store.project.maps.map((x) => x.filename);
+});
 </script>
 
 <template>
   <v-container>
     <div>{{ store.project.maps.length }}</div>
-    <div>{{ store.project.maps[0].data }}</div>
+    <div v-for="data in maps">{{ data }}</div>
   </v-container>
 </template>
 
