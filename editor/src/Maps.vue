73205e8959b0dd20a25ff004ccafd334fc44a6f0
Matt Pitts <matt.pitts@nuqleous.com>
2023-12-22 23:25:02 -0600

    tabs example

--- editor/src/Maps.vue
+++ editor/src/Maps.vue
@@ -2,10 +2,13 @@
 import { computed } from "vue";
 import { useStore } from "./store";
 import { lingoValueToString } from "./ccsr/parser/parser";
+import { reactive } from "vue";
+import { ref } from "vue";
 const store = useStore();
 const maps = computed(() => {
   return store.project.maps.map((x) => x.filename);
 });
+const tab = ref("two");
 </script>
 
 <template>
@@ -16,6 +19,20 @@ const maps = computed(() => {
     </v-col>
     <v-col cols="6" style="background-color: blue">
       <!-- Content for the right half goes here -->
+      <v-card style="height: 100%">
+        <v-tabs v-model="tab" bg-color="primary">
+          <v-tab value="one">Item One</v-tab>
+          <v-tab value="two">Item Two</v-tab>
+          <v-tab value="three">Item Three</v-tab>
+        </v-tabs>
+        <v-card-text>
+          <v-window v-model="tab">
+            <v-window-item value="one"> One </v-window-item>
+            <v-window-item value="two"> Two </v-window-item>
+            <v-window-item value="three"> Three </v-window-item>
+          </v-window>
+        </v-card-text>
+      </v-card>
     </v-col>
   </v-row>
 </template>
