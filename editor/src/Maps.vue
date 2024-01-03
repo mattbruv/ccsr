37f08bcd911e9a58d48199a2f58e2678b5e723be
Matt P <5638426+mattbruv@users.noreply.github.com>
2024-01-02 23:02:13 -0600

    mess aroudn with stuff

--- editor/src/Maps.vue
+++ editor/src/Maps.vue
@@ -2,77 +2,31 @@
 import { ref } from "vue";
 import { useStore } from "./store";
 import { VDataTable } from "vuetify/labs/components";
+import { storeToRefs } from "pinia";
+import { GameMap } from "./ccsr/renderer/types";
 import { computed } from "vue";
+
 const store = useStore();
+const { gameMaps, gameObjects } = storeToRefs(store);
 
 const search = ref("");
 const headers = [
   {
     align: "start",
     key: "name",
-    sortable: false,
-    title: "Dessert (100g serving)",
+    sortable: true,
+    title: "Map",
   },
-  { key: "calories", title: "Calories" },
-  { key: "fat", title: "Fat (g)" },
-  { key: "carbs", title: "Carbs (g)" },
-  { key: "protein", title: "Protein (g)" },
-  { key: "iron", title: "Iron (%)" },
+  { key: "objects", title: "Objects" },
+  { key: "oob", title: "Render OOB" },
 ];
-const desserts = computed(() => [
-  {
-    name: "Frozen Yogurt",
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    iron: 1,
-  },
-  {
-    name: "Ice cream sandwich",
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    iron: 1,
-  },
-  {
-    name: "Eclair",
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    iron: 7,
-  },
-  {
-    name: "Cupcake",
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    iron: 8,
-  },
-  {
-    name: "Gingerbread",
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    iron: 16,
-  },
-  {
-    name: "Jelly bean",
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    iron: 0,
-  },
-]);
+
+const foo = computed(() => gameMaps.value[0]);
 </script>
 
 <template>
-  <v-card flat title="Nutrition">
+  {{ foo }}
+  <v-card flat title="Maps">
     <template v-slot:text>
       <v-text-field
         v-model="search"
@@ -81,14 +35,21 @@ const desserts = computed(() => [
         single-line
         variant="outlined"
         hide-details
-      ></v-text-field>
+      >
+      </v-text-field>
     </template>
-
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :search="search"
-    ></v-data-table>
+    <v-data-table :headers="headers" :items="gameMaps" :search="search">
+      <template v-slot:item="{ item }: { item: GameMap }">
+        <tr>
+          <td>{{ item.name }}</td>
+          <td>
+            <v-checkbox
+              :v-bind="item.renderSettings.renderOutOfBounds"
+            ></v-checkbox>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
   </v-card>
 </template>
 
