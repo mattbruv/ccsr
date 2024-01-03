d36dc640f5a7b297dfe2221c2cc004da50e6f6a5
Matt P <5638426+mattbruv@users.noreply.github.com>
2024-01-02 21:56:03 -0600

    start this crap

--- editor/src/Maps.vue
+++ editor/src/Maps.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import { useStore } from "./store";
+import { VDataTable } from "vuetify/labs/components";
 const store = useStore();
 
-const maps = computed(() => store.gameMaps);
+type MapTableData = {
+  name: string;
+  renderBorder: boolean;
+  renderGrid: boolean;
+  renderOutOfBounds: boolean;
+};
+
+const items = [
+  {
+    foo: "bar",
+    baz: "dog",
+  },
+  {
+    foo: "rar",
+    baz: "bog",
+  },
+];
 </script>
 
 <template>
-  <div v-for="foo of maps">{{ foo.name }}</div>
-  <div>bar</div>
+  <div>
+    <v-data-table :items="items"></v-data-table>
+  </div>
 </template>
 
 <style scoped></style>
