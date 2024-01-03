58ce6257ada068786185f7a51d5f3e3d88fd9b95
Matt Pitts <matt.pitts@nuqleous.com>
2024-01-03 13:11:21 -0600

    border tests

--- editor/src/ccsr/renderer/helpers.ts
+++ editor/src/ccsr/renderer/helpers.ts
@@ -1,62 +1,62 @@
-import * as PIXI from "pixi.js"
-import { GameMapRenderData, GameObject, GameObjectRenderData } from "./types"
+import * as PIXI from "pixi.js";
+import { GameMapRenderData, GameObject, GameObjectRenderData } from "./types";
 
 const MAP_WIDTH_PIXELS = 32 * 13;
 const MAP_HEIGHT_PIXELS = 32 * 10;
 
-
-export function newGameObjectRenderData(gameObject: GameObject, texture: PIXI.Texture): GameObjectRenderData {
-    const data: GameObjectRenderData = {
-        hash: "",
-        sprite: new PIXI.TilingSprite(texture),
-    }
-    return data
+export function newGameObjectRenderData(
+  gameObject: GameObject,
+  texture: PIXI.Texture
+): GameObjectRenderData {
+  const data: GameObjectRenderData = {
+    hash: "",
+    sprite: new PIXI.TilingSprite(texture),
+  };
+  return data;
 }
 
 export function newGameMapRenderData(name: string): GameMapRenderData {
-    const data: GameMapRenderData = {
-        mask: new PIXI.Graphics(),
-        border: new PIXI.Graphics(),
-        mapContainer: new PIXI.Container(),
-        objectContainer: new PIXI.Container(),
-        grid: new PIXI.Graphics(),
-    }
-    //data.mapContainer.cacheAsBitmap = true;
+  const data: GameMapRenderData = {
+    mask: new PIXI.Graphics(),
+    border: new PIXI.Graphics(),
+    mapContainer: new PIXI.Container(),
+    objectContainer: new PIXI.Container(),
+    grid: new PIXI.Graphics(),
+  };
+  //data.mapContainer.cacheAsBitmap = true;
 
-    // Draw mask object
-    data.mask.beginFill(0xffffff)
-    data.mask.drawRect(0, 0, MAP_WIDTH_PIXELS, MAP_HEIGHT_PIXELS)
-    data.mask.endFill()
+  // Draw mask object
+  data.mask.beginFill(0xffffff);
+  data.mask.drawRect(0, 0, MAP_WIDTH_PIXELS, MAP_HEIGHT_PIXELS);
+  data.mask.endFill();
 
-    // Draw the border graphic
-    data.border.lineStyle(2, 0xff00ff, 1);
-    data.border.drawRect(0, 0, MAP_WIDTH_PIXELS, MAP_HEIGHT_PIXELS)
+  // Draw the border graphic
+  data.border.lineStyle(2, 0xff00ff, 1);
+  data.border.drawRect(1, 1, MAP_WIDTH_PIXELS - 1, MAP_HEIGHT_PIXELS - 2);
 
-    // Add the container for our game objects
-    data.mapContainer.addChild(data.objectContainer)
-    data.mapContainer.addChild(data.border)
+  // Add the container for our game objects
+  data.mapContainer.addChild(data.objectContainer);
+  data.mapContainer.addChild(data.border);
 
-    // The mask must be a child in the subtree of its parent to work
-    data.mapContainer.addChild(data.mask)
+  // The mask must be a child in the subtree of its parent to work
+  data.mapContainer.addChild(data.mask);
 
-
-    return data
+  return data;
 }
 
 export function getTextureName(texture: string): string {
-    let name = texture.toLowerCase()
-    if (name.startsWith("tile"))
-        name = name.replace(".x", "")
-    return name
+  let name = texture.toLowerCase();
+  if (name.startsWith("tile")) name = name.replace(".x", "");
+  return name;
 }
 
-export function getMapLocation(name: string): { x: number, y: number } {
-    if (name.length == 4) {
-        const X = parseInt(name.slice(0, 2))
-        const Y = parseInt(name.slice(2, 4))
-        const x = X * MAP_WIDTH_PIXELS
-        const y = Y * MAP_HEIGHT_PIXELS
-        return { x, y }
-    }
-    return { x: 0, y: 0 }
+export function getMapLocation(name: string): { x: number; y: number } {
+  if (name.length == 4) {
+    const X = parseInt(name.slice(0, 2));
+    const Y = parseInt(name.slice(2, 4));
+    const x = X * MAP_WIDTH_PIXELS;
+    const y = Y * MAP_HEIGHT_PIXELS;
+    return { x, y };
+  }
+  return { x: 0, y: 0 };
 }
--- editor/src/ccsr/renderer/renderer.ts
+++ editor/src/ccsr/renderer/renderer.ts
@@ -1,8 +1,18 @@
 import * as PIXI from "pixi.js";
 import { ImageFile } from "../types";
 import { Viewport } from "pixi-viewport";
-import { GameMap, GameMapRenderData, GameObject, GameObjectRenderData } from "./types"
-import { newGameObjectRenderData, newGameMapRenderData, getTextureName, getMapLocation } from "./helpers";
+import {
+  GameMap,
+  GameMapRenderData,
+  GameObject,
+  GameObjectRenderData,
+} from "./types";
+import {
+  newGameObjectRenderData,
+  newGameMapRenderData,
+  getTextureName,
+  getMapLocation,
+} from "./helpers";
 
 class CcsrRenderer {
   public app = new PIXI.Application<HTMLCanvasElement>();
@@ -30,10 +40,10 @@ class CcsrRenderer {
 
   public reset() {
     PIXI.utils.destroyTextureCache();
-    PIXI.utils.clearTextureCache()
-    this.viewport.removeChildren()
-    this.gameMaps.clear()
-    this.gameObjects.clear()
+    PIXI.utils.clearTextureCache();
+    this.viewport.removeChildren();
+    this.gameMaps.clear();
+    this.gameObjects.clear();
   }
 
   public addView(div: HTMLDivElement): void {
@@ -47,7 +57,7 @@ class CcsrRenderer {
   }
 
   public unloadImages() {
-    // It says: 
+    // It says:
     // Only intended for development purposes.
     // This will wipe the resolver and caches.
     // You will need to reinitialize the Asset
@@ -71,17 +81,18 @@ class CcsrRenderer {
   }
 
   public renderWorld(gameMaps: GameMap[], gameObjects: GameObject[]) {
-
     for (const gameObject of gameObjects) {
-      let entry = this.gameObjects.get(gameObject.id)
+      let entry = this.gameObjects.get(gameObject.id);
       if (!entry) {
-        const textureName = getTextureName(gameObject.data.member ?? "missing_texture")
+        const textureName = getTextureName(
+          gameObject.data.member ?? "missing_texture"
+        );
         const texture = PIXI.Texture.from(textureName);
-        entry = newGameObjectRenderData(gameObject, texture)
-        this.gameObjects.set(gameObject.id, entry)
+        entry = newGameObjectRenderData(gameObject, texture);
+        this.gameObjects.set(gameObject.id, entry);
       }
 
-      this.renderGameObject(entry, gameObject)
+      this.renderGameObject(entry, gameObject);
     }
 
     // Render each game map in the world
@@ -91,90 +102,101 @@ class CcsrRenderer {
 
       // Create this map if it doesn't exist
       if (!entry) {
-        entry = newGameMapRenderData(gameMap.name)
-        this.gameMaps.set(gameMap.name, entry)
-        this.viewport.addChild(entry.mapContainer)
+        entry = newGameMapRenderData(gameMap.name);
+        this.gameMaps.set(gameMap.name, entry);
+        this.viewport.addChild(entry.mapContainer);
         // Set the map's X/Y position to correct spot
-        const location = getMapLocation(gameMap.name)
-        entry.mapContainer.position.set(location.x, location.y)
-        entry.mapContainer.mask = entry.mask
+        const location = getMapLocation(gameMap.name);
+        entry.mapContainer.position.set(location.x, location.y);
+        entry.mapContainer.mask = entry.mask;
       }
 
-      this.renderGameMap(entry, gameMap)
+      this.renderGameMap(entry, gameMap);
 
-      const objectsInMap = gameObjects.filter(x => x.mapName === gameMap.name);
+      const objectsInMap = gameObjects.filter(
+        (x) => x.mapName === gameMap.name
+      );
 
       // Add each game object in this map to its children
       for (const gameObject of objectsInMap) {
-        const obj = this.gameObjects.get(gameObject.id)
+        const obj = this.gameObjects.get(gameObject.id);
         if (obj) {
-          entry.objectContainer.addChild(obj.sprite)
+          entry.objectContainer.addChild(obj.sprite);
         }
       }
-
     }
 
-    console.log(this)
+    console.log(this);
   }
 
   private renderGameMap(entry: GameMapRenderData, map: GameMap) {
     // disable caching as bitmap while we make changes to our object
-    entry.mapContainer.cacheAsBitmap = false
-    entry.border.visible = map.renderSettings.renderBorder
-    entry.grid.visible = map.renderSettings.renderGrid
+    entry.mapContainer.cacheAsBitmap = false;
+    entry.border.visible = map.renderSettings.renderBorder;
+    entry.grid.visible = map.renderSettings.renderGrid;
     // Do not mask the map container if we want to render out of bounds objects
-    entry.mapContainer.mask = (map.renderSettings.renderOutOfBounds) ? null : entry.mask
+    if (map.renderSettings.renderOutOfBounds) {
+      entry.mapContainer.mask = null;
+      entry.mask.visible = false;
+    } else {
+      entry.mapContainer.mask = entry.mask;
+      entry.mask.visible = true;
+    }
 
     // Set cache as bitmap to true to reset our cached texture again
-    entry.mapContainer.cacheAsBitmap = true
+    entry.mapContainer.cacheAsBitmap = true;
   }
 
-  private renderGameObject(entry: GameObjectRenderData, gameObject: GameObject) {
-    const hash = JSON.stringify(gameObject)
+  private renderGameObject(
+    entry: GameObjectRenderData,
+    gameObject: GameObject
+  ) {
+    const hash = JSON.stringify(gameObject);
 
     // If our sprite is already rendered correctly, don't do anything
-    if (entry.hash === hash)
-      return
+    if (entry.hash === hash) return;
 
     if (gameObject.data.width) {
-      entry.sprite.width = gameObject.data.width
+      entry.sprite.width = gameObject.data.width;
     }
     if (gameObject.data.height) {
-      entry.sprite.height = gameObject.data.height
+      entry.sprite.height = gameObject.data.height;
     }
 
     if (gameObject.data.location) {
-      const x = gameObject.data.location.x ?? 0
-      const y = gameObject.data.location.y ?? 0
-      const WSHIFT = gameObject.data.WSHIFT ?? 0
-      const HSHIFT = gameObject.data.HSHIFT ?? 0
+      const x = gameObject.data.location.x ?? 0;
+      const y = gameObject.data.location.y ?? 0;
+      const WSHIFT = gameObject.data.WSHIFT ?? 0;
+      const HSHIFT = gameObject.data.HSHIFT ?? 0;
       const posX = x * 16 + WSHIFT;
       const posY = y * 16 + HSHIFT;
       entry.sprite.position.set(posX, posY);
     }
 
     if (gameObject.data.member) {
-      const textureName = getTextureName(gameObject.data.member ?? "missing_texture")
+      const textureName = getTextureName(
+        gameObject.data.member ?? "missing_texture"
+      );
       const texture = PIXI.Texture.from(textureName);
-      entry.sprite.texture = texture
+      entry.sprite.texture = texture;
 
       // If the game object is not tiling, set the anchor to the middle
       // And make its texture stretch to fill the entire area
       if (!textureName.includes("tile")) {
-        entry.sprite.anchor.set(0.5)
-        entry.sprite.width = entry.sprite.texture.width
-        entry.sprite.height = entry.sprite.texture.height
+        entry.sprite.anchor.set(0.5);
+        entry.sprite.width = entry.sprite.texture.width;
+        entry.sprite.height = entry.sprite.texture.height;
       }
     }
 
     if (gameObject.data.data?.item?.visi) {
       const { visiAct, visiObj } = gameObject.data.data?.item?.visi;
       if (visiAct || visiObj) {
-        entry.sprite.alpha = 0.5
+        entry.sprite.alpha = 0.5;
       }
     }
 
-    entry.hash = hash
+    entry.hash = hash;
   }
 }
 
