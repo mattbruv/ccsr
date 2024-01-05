f7d834a99c7d28400266842c35f66637c8c5b742
Matt P <5638426+mattbruv@users.noreply.github.com>
2024-01-04 20:25:54 -0600

    change load layout

--- editor/src/Load.vue
+++ editor/src/Load.vue
@@ -30,36 +30,32 @@ watch(selectedEpisode, async () => {
   </v-container>
   <v-container v-else>
     <v-row>
-      <v-col cols="12" md="6">
-        <v-card style="padding: 1rem">
-          <v-card-title>Load From Episode</v-card-title>
-          <v-card-subtitle>
-            Load data from existing an existing episode.
-          </v-card-subtitle>
-          <div style="padding-top: 1rem">
-            <v-select
-              label="Select Episode"
-              v-model="selectedEpisode"
-              :items="episodes"
-            ></v-select>
-          </div>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card style="padding: 1rem" variant="tonal">
-          <v-card-title>Load From Project File</v-card-title>
-          <v-card-subtitle>
-            Load a previously exported Project File (.ccsr.zip)
-          </v-card-subtitle>
-          <v-file-input
-            label="Select Project File"
-            variant="underlined"
-            :prepend-icon="'mdi-upload'"
-            accept=".ccsr.zip"
-            append-inner-icon="mdi-folder-zip"
-          ></v-file-input>
-        </v-card>
-      </v-col>
+      <v-card style="padding: 1rem; width: 100%; margin: 1rem">
+        <v-card-title>Load From Episode</v-card-title>
+        <v-card-subtitle>
+          Load data from existing an existing episode.
+        </v-card-subtitle>
+        <div style="padding-top: 1rem">
+          <v-select
+            label="Select Episode"
+            v-model="selectedEpisode"
+            :items="episodes"
+          ></v-select>
+        </div>
+      </v-card>
+      <v-card style="padding: 1rem; width: 100%; margin: 1rem" variant="tonal">
+        <v-card-title>Load From Project File</v-card-title>
+        <v-card-subtitle>
+          Load a previously exported Project File (.ccsr.zip)
+        </v-card-subtitle>
+        <v-file-input
+          label="Select Project File"
+          variant="underlined"
+          :prepend-icon="'mdi-upload'"
+          accept=".ccsr.zip"
+          append-inner-icon="mdi-folder-zip"
+        ></v-file-input>
+      </v-card>
     </v-row>
   </v-container>
 </template>
