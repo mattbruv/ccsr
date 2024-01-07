a902e2441ea834535a994e29b0e73c9e835a0b7c
Matt P <5638426+mattbruv@users.noreply.github.com>
2024-01-06 18:29:12 -0600

    add some object headers

--- editor/src/ccsr/mapEditor/Objects.vue
+++ editor/src/ccsr/mapEditor/Objects.vue
@@ -1,15 +1,43 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
 import { useStore } from "../../store";
+import { VDataTable } from "vuetify/labs/components";
 import { computed } from "vue";
+
+type ReadonlyHeaders = VDataTable["headers"];
+
+const headers: ReadonlyHeaders = [
+  {
+    title: "Image",
+  },
+  {
+    title: "Member",
+    value: "data.member",
+  },
+  {
+    title: "Type",
+    value: "data.data.item.type",
+  },
+  {
+    title: "messages",
+    value: "data.data.message.length",
+  },
+  {
+    title: "Action",
+  },
+];
 
 const store = useStore();
 const { gameMaps, gameObjects, selectedMap } = storeToRefs(store);
+
+const mapObjects = computed(() =>
+  gameObjects.value.filter((x) => x.mapName === selectedMap.value?.name)
+);
 </script>
 
 <template>
   <v-container>
-    <div>objects</div>
+    <v-data-table :headers="headers" :items="mapObjects"></v-data-table>
   </v-container>
 </template>
 
