074513f5cf2f07dce116f216b5c41e3a8fc4a6f7
Matt Pitts <matt.pitts@nuqleous.com>
2024-02-19 16:30:14 -0600

    bind texture

--- editor/src/ccsr/mapEditor/ObjectEditor.vue
+++ editor/src/ccsr/mapEditor/ObjectEditor.vue
@@ -2,6 +2,9 @@
 import { storeToRefs } from "pinia";
 import { useStore } from "../../store";
 import { computed } from "vue";
+import { lingoValueToString } from "../parser/print";
+import { mapObjectToLingo } from "../game/toLingo";
+import { GameObject } from "../renderer/types";
 
 const store = useStore();
 const { gameMaps, gameObjects, selectedMap } = storeToRefs(store);
@@ -9,12 +12,42 @@ const { gameMaps, gameObjects, selectedMap } = storeToRefs(store);
 const selectedObject = computed(() => {
   return gameObjects.value.find((x) => x.id === store.selectedObjectId);
 });
+
+const json = computed((): string => {
+  if (!selectedObject.value) return "";
+  const lingoObject = mapObjectToLingo(selectedObject.value.data);
+  return lingoValueToString(lingoObject, true, true);
+});
+
+const memberTextures = computed(() => {
+  return store.imageFiles
+    .map((x) => x.filename)
+    .sort((a, b) => a.localeCompare(b));
+});
+
+function render() {
+  store.render();
+}
 </script>
 
 <template>
   <v-container>
     <div v-if="!selectedObject">No object selected!</div>
-    <div v-else>{{ selectedObject.id }}</div>
+    <div v-else>
+      {{ selectedObject.id }}
+      <v-autocomplete
+        :items="memberTextures"
+        v-model="selectedObject.data.member"
+        @update:model-value="render"
+        label="Member Texture"
+      >
+      </v-autocomplete>
+      <div>
+        <code>
+          {{ json }}
+        </code>
+      </div>
+    </div>
   </v-container>
 </template>
 
